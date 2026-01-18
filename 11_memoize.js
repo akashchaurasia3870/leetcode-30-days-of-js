c0963ff3f97f830a2e4c5856c02303a82fd19a27
Akash Chaurasia <akash.chaurasia3870@gmail.com>
2026-01-18 20:35:47 +0530

    sol: problam 10,11

---
+++ 11_memoize.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    let cache = {};
+    return function(...args) {
+        let key = args.join(",");
+        if(key in cache) return cache[key];
+        return (cache[key]=fn(...args));
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
